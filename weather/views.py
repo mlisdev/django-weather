508050d2e108cb7a6b7a2ecd06dd622c671113f4
Stephanie <Rinehart>
2020-12-30 17:41:15 -0500

    inoit

---
+++ weather/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+import requests
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=YOUR_APP_KEY'
+    city = 'Las Vegas'
+    city_weather = requests.get(url.format(city)).json()  #request the API data and convert the JSON to Python data types
+    
+    weather = {
+        'city' : city,
+        'temperature' : city_weather['main']['temp'],
+        'description' : city_weather['weather'][0]['description'],
+        'icon' : city_weather['weather'][0]['icon']
+    }
+
+    context = {'weather': weather }
+
+    return render(request, 'weather/index.html') #returns the index.html template
