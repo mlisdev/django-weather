dc53e20eff99f982c78b9a5ec7d9063ed11e421e
Stephanie <Rinehart>
2020-12-30 18:22:32 -0500

    working app

--- weather/models.py
+++ weather/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
-# Create your models here.
-
 class City(models.Model):
     name = models.CharField(max_length=25)
 
-    def _str_(self): #show the actual city name on the dashboard
+    def __str__(self): #show the actual city name on the dashboard
         return self.name
 
     class Meta: #show the plural of city as cities instead of citys
--- weather/views.py
+++ weather/views.py
@@ -1,18 +1,37 @@
 from django.shortcuts import render
 import requests
+from .models import City
+from .forms import CityForm
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=YOUR_APP_KEY'
-    city = 'Las Vegas'
-    city_weather = requests.get(url.format(city)).json()  #request the API data and convert the JSON to Python data types
+    cities = City.objects.all() #return all the cities in the database
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=664db2ef81a342a1aadedc56f07c4a57'
+
+    if request.method == 'POST': # only true if form is submitted
+        form = CityForm(request.POST) # add actual request data to form for processing
+        form.save() # will validate and save if validate
+
+    form = CityForm()
+
+    weather_data = []
+
+    for city in cities:
+
+        response = requests.get(url.format(city.name))
+        if response.status_code == 404:
+            continue
+        city_weather = response.json()
+        
+        weather = {
+            'city' : city,
+            'temperature' : city_weather['main']['temp'],
+            'description' : city_weather['weather'][0]['description'],
+            'icon' : city_weather['weather'][0]['icon']
+        }
+
+        weather_data.append(weather) #add the data for the current city into our list
     
-    weather = {
-        'city' : city,
-        'temperature' : city_weather['main']['temp'],
-        'description' : city_weather['weather'][0]['description'],
-        'icon' : city_weather['weather'][0]['icon']
-    }
+    context = {'weather_data' : weather_data, 'form' : form}
 
-    context = {'weather': weather }
-
-    return render(request, 'weather/index.html') #returns the index.html template
+    return render(request, 'weather/index.html', context) #returns the index.html template
