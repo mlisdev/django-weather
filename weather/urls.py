f36aa4141f506910772b77f7bd90d64572e9aad9
Stephanie <Rinehart>
2020-12-30 18:43:38 -0500

    modify index

--- weather/urls.py
+++ weather/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index), #the path for our index view
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
